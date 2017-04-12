e8b2a878d6405702f2fdd41e8b1782982d3ca02a
Thomas Orozco <thomas@orozco.fr>
2017-04-12 14:20:57 +0200

    Security scanning fields

--- lib/aptible/api/image.rb
+++ lib/aptible/api/image.rb
@@ -2,6 +2,7 @@ module Aptible
   module Api
     class Image < Resource
       belongs_to :app
+      has_many :operations
 
       field :id
       field :git_repo
@@ -9,6 +10,9 @@ module Aptible
       field :docker_repo
       field :docker_ref
       field :dualstack_hint
+      field :platform
+      field :release
+      field :scan
       field :created_at, type: Time
       field :updated_at, type: Time
     end
