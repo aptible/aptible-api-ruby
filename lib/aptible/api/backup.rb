96133455dfaefaefaa20db00aebbcd33f55963bb
Robert Fairhead <robertfairhead@users.noreply.github.com>
2020-06-24 10:22:22 -0500

    Add ability to find deleted database records from backups (#99)
    Part of https://aptible.atlassian.net/browse/DP-282
    
    Adds a method to allow looking up the record for a deleted database from
    a backup. This will be used in sweetness to allow for copying backups
    whose database has already been deleted.

--- lib/aptible/api/backup.rb
+++ lib/aptible/api/backup.rb
@@ -14,6 +14,11 @@ module Aptible
       field :aws_region
       field :created_at, type: Time
       field :updated_at, type: Time
+
+      def database_with_deleted
+        db_id = links['database'].href.split('/').last
+        Database.find(db_id, with_deleted: true, token: token, headers: headers)
+      end
     end
   end
 end
