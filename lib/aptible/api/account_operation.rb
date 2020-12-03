d117bd02bb16b354317bb76709d591f60a65370e
Josh Raker <josh.raker@aptible.com>
2020-12-03 07:08:36 -0500

    Update operation types

--- lib/aptible/api/account_operation.rb
+++ lib/aptible/api/account_operation.rb
@@ -2,51 +2,7 @@ require 'aptible/auth'
 
 module Aptible
   module Api
-    class OperationBase < Resource
-      # belongs_to :account
-      belongs_to :resource
-
-      # has_many :ssh_portal_connections
-      # has_many :ephemeral_sessions
-
-      field :id
-      field :type
-      field :status
-      field :cancelled
-      field :aborted
-      field :git_ref
-      field :docker_ref
-      field :certificate
-      field :private_key
-      field :env
-      field :container_size
-      field :container_count
-      field :disk_size
-      field :command
-      # field :destination_region
-      field :interactive
-      # belongs_to :destination_account
-      field :user_name
-      field :user_email
-      field :immediate
-      field :created_at, type: Time
-      field :updated_at, type: Time
-
-      def user
-        auth = Aptible::Auth::User.new(token: token, headers: headers)
-        auth.find_by_url(links['user'].href)
-      rescue StandardError
-        nil
-      end
-
-      def succeeded?
-        status == 'succeeded'
-      end
-
-      def failed?
-        status == 'failed'
-      end
-
+    class AccountOperation < Operation
       def with_ssh_cmd(private_key_file, host: nil, port: nil, host_key: nil)
         # We expect that the public key will be found next to the private key,
         # which is also what SSH itself expects. If that's not the case, then
