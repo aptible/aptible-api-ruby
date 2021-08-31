a2bcca898b0f6a4654775556bb680ec030c6eb73
Robert Fairhead <robert.fairhead@aptible.com>
2021-08-31 11:24:33 -0500

    Add drained attribute to InstanceLayerMembership

--- lib/aptible/api/instance_layer_membership.rb
+++ lib/aptible/api/instance_layer_membership.rb
@@ -3,6 +3,8 @@ module Aptible
     class InstanceLayerMembership < Resource
       belongs_to :stack_layer
       belongs_to :aws_instance
+
+      field :drained, type: Aptible::Resource::Boolean
     end
   end
 end
