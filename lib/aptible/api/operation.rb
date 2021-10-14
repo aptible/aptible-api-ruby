64cd13ccb116c5950a6d43082e50896a068fa50c
Ashley Mathew <almathew@users.noreply.github.com>
2021-10-14 19:43:30 -0400

    Add instance_profile (#131)

--- lib/aptible/api/operation.rb
+++ lib/aptible/api/operation.rb
@@ -32,6 +32,7 @@ module Aptible
       field :stack_settings
       field :created_at, type: Time
       field :updated_at, type: Time
+      field :instance_profile
 
       def user
         auth = Aptible::Auth::User.new(token: token, headers: headers)
