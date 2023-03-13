c1ca07cc794889f921f71497f76c29daf68a35d8
Madhu <madhuravius@protonmail.com>
2023-03-13 13:30:41 -0400

    chore: added a few more fields that were since added

--- lib/aptible/api/active_plan.rb
+++ lib/aptible/api/active_plan.rb
@@ -7,11 +7,15 @@ module Aptible
       field :plan_id
       field :container_memory_limit_mb
       field :cpu_allowed_profiles
-      field :disk_limit_mib
+      field :disk_limit_gb
       field :ephemeral_session_limit
       field :environment_limit
       field :vhost_limit
       field :compliance_dashboard_access
+      field :allowed_plans
+      field :included_container_mb
+      field :included_disk_gb
+      field :included_vhosts
       field :organization_id
       field :created_at, type: Time
       field :updated_at, type: Time
