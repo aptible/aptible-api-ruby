7a3c0d7d4cd4177c94b65df559dcf75d93eeb044
Madhu <madhuravius@protonmail.com>
2023-05-11 21:14:46 -0400

    chore: added more fields from codescanning

--- lib/aptible/api/code_scan_result.rb
+++ lib/aptible/api/code_scan_result.rb
@@ -5,11 +5,17 @@ module Aptible
       belongs_to :operation
 
       field :id
+      field :dockerfile_data
       field :dockerfile_present
+      field :procfile_data
       field :procfile_present
+      field :aptible_yml_data
       field :aptible_yml_present
       field :git_ref
       field :git_commit
+      field :languages_detected
+      field :total_repo_size_bytes
+      field :total_file_count
 
       field :created_at, type: Time
       field :updated_at, type: Time
