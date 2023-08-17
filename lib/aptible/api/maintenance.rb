fc6e511ac142139ac2793b4cd14f05b3b0c3db66
Madhu <madhuravius@protonmail.com>
2023-08-16 22:34:39 -0400

    chore: added awkward maintenance resources

---
+++ lib/aptible/api/maintenance.rb
@@ -0,0 +1,27 @@
+module Aptible
+  module Api
+    class MaintenanceApp < Resource
+      field :id
+      field :handle
+      field :created_at
+      field :updated_at
+      field :status
+      field :maintenance_deadline
+      def self.collection_href
+        "/maintenances/#{basename}"
+      end
+    end
+    class MaintenanceDatabase < Resource
+      field :id
+      field :handle
+      field :created_at
+      field :updated_at
+      field :status
+      field :maintenance_deadline
+      def self.collection_href
+        "/maintenances/#{basename}"
+      end
+    end
+  end
+end
+
