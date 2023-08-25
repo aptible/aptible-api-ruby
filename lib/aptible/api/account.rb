eb1da18a5252ebedd239c72ea2151735c53df578
Ann Guilinger <ann.guilinger@aptible.com>
2023-08-25 12:15:10 -0400

    Ensure vhosts and backup retention policies are paginated

--- lib/aptible/api/account.rb
+++ lib/aptible/api/account.rb
@@ -16,6 +16,8 @@ module Aptible
       has_many :permissions
       has_many :metric_drains
       has_many :backup_retention_policy
+      has_many :vhosts
+      has_many :backup_retention_policies
       embeds_many :log_drains
 
       field :id
--- lib/aptible/api/version.rb
+++ lib/aptible/api/version.rb
@@ -1,5 +1,5 @@
 module Aptible
   module Api
-    VERSION = '1.4.7'.freeze
+    VERSION = '1.4.8'.freeze
   end
 end
