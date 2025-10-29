5744a1dda8c347d454ccf37a73e99f280d604059
Madhusudan Ravi <madhu.ravi@aptible.com>
2025-10-28 21:36:58 -0400

    feat: exposing endpoints for testing

---
+++ lib/aptible/api/external_aws_account.rb
@@ -0,0 +1,36 @@
+module Aptible
+  module Api
+    class ExternalAwsAccount < Resource
+      has_many :external_aws_resources
+
+      field :id
+      field :organization_id
+      field :aws_account_id
+      field :account_name
+      field :aws_region_primary
+      field :discovery_enabled, type: Aptible::Resource::Boolean
+      field :discovery_frequency
+      field :last_discovery_at, type: Time
+      field :status
+      field :created_by_user_email
+      field :created_by_user_name
+      field :created_by_user_id
+      field :deleted_at, type: Time
+      field :created_at, type: Time
+      field :updated_at, type: Time
+    end
+
+    def organization_url
+      links['organization'].href
+    end
+
+    def organization
+      return @organization if @organization
+
+      auth = Aptible::Auth::Organization.new(token: token, headers: headers)
+      @organization = auth.find_by_url(organization_url)
+    end
+  end
+end
+
+
