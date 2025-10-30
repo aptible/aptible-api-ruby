f30af3b373c6aeaca84a5be0b0e91aa7155e598a
Madhusudan Ravi <madhu.ravi@aptible.com>
2025-10-30 11:21:03 -0400

    chore: included app db conn with has many

--- lib/aptible/api/external_aws_resource.rb
+++ lib/aptible/api/external_aws_resource.rb
@@ -2,6 +2,7 @@ module Aptible
   module Api
     class ExternalAwsResource < Resource
       belongs_to :external_aws_account
+      has_many :app_database_connection
 
       field :id
       field :resource_type
