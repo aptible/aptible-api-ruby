8299f5ae09bebf78e5dd33caf689e0b8e7289ab2
T Van Doren <210452321+tvdaptible@users.noreply.github.com>
2025-12-11 13:25:31 -0800

    lint roller

--- lib/aptible/api/external_aws_account_check_response.rb
+++ lib/aptible/api/external_aws_account_check_response.rb
@@ -1,6 +1,10 @@
 module Aptible
   module Api
-    ExternalAwsAccountCheckResponse = Struct.new("ExternalAwsAccountCheckResponse", :state, :checks, keyword_init: true)
-    ExternalAwsAccountCheck = Struct.new("ExternalAwsAccountCheck", :check_name, :state, :details, keyword_init: true)
+    ExternalAwsAccountCheckResponse = Struct.new(
+      'ExternalAwsAccountCheckResponse', :state, :checks, keyword_init: true
+    )
+    ExternalAwsAccountCheck = Struct.new(
+      'ExternalAwsAccountCheck', :check_name, :state, :details, keyword_init: true
+    )
   end
 end
