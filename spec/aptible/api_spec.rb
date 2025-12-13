a118e0a5dfe1e447bf43498d1f98a0845ed995ff
T Van Doren <210452321+tvdaptible@users.noreply.github.com>
2025-12-12 18:21:25 -0800

    ensure tests work even if APTIBLE_API_ROOT_URL env var is set

--- spec/aptible/api_spec.rb
+++ spec/aptible/api_spec.rb
@@ -6,12 +6,17 @@ describe Aptible::Api do
   it 'should have a configurable root_url' do
     config = described_class.configuration
     expect(config).to be_a GemConfig::Configuration
-    expect(config.root_url).to eq 'https://api.aptible.com'
+    with_env 'APTIBLE_API_ROOT_URL', nil do
+      load 'aptible/api.rb'
+      config.reset
+      expect(config.root_url).to eq 'https://api.aptible.com'
+    end
   end
 
-  pending 'uses ENV["APTIBLE_API_ROOT_URL"] if defined' do
+  it 'uses ENV["APTIBLE_API_ROOT_URL"] if defined' do
     config = described_class.configuration
     with_env 'APTIBLE_API_ROOT_URL', 'http://foobar.com' do
+      load 'aptible/api.rb'
       config.reset
       expect(config.root_url).to eq 'http://foobar.com'
     end
