83267334ed1b1dcba559120fa40b75871d2cd547
Taylor Meek <taylor.meek@aptible.com>
2025-12-16 15:23:47 -0800

    sc-35229: user and actor for revoke and create

--- lib/aptible/api/ai_token.rb
+++ lib/aptible/api/ai_token.rb
@@ -11,6 +11,22 @@ module Aptible
       field :last_used_at
       field :revoked_at
       field :gateway_url
+      # User (on whose behalf) details
+      field :created_by_user_id
+      field :created_by_user_name
+      field :created_by_user_email
+      # Actor (who performed) details
+      field :created_by_actor_id
+      field :created_by_actor_name
+      field :created_by_actor_email
+      # Revoked by user details
+      field :revoked_by_user_id
+      field :revoked_by_user_name
+      field :revoked_by_user_email
+      # Revoked by actor details
+      field :revoked_by_actor_id
+      field :revoked_by_actor_name
+      field :revoked_by_actor_email
 
       # Note: The 'token' field from API response is accessible via
       # attributes['token'] to avoid conflict with aptible-resource's
